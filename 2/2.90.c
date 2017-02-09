6f19d3fe849b7e84c379bfe4373da3d88dd0d2a9
jetou <1021644861@qq.com>
2017-02-09 19:40:52 +0800

    dsa

---
+++ 2/2.90.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+float fpwr2(int x)
+{
+    unsigned exp, frac;
+    unsigned u;
+
+    if(x < -149){
+        exp = 0;
+        frac = 0;
+    }else if(x < -126){
+        exp = 0;
+        frac = 1 << (x+149);
+    }else if(x < 128){
+        exp = x + 127;
+        frac = 0;
+    }else{
+        exp = 255;
+        frac = 0;
+    }
+
+    u = exp<<23|frac;
+
+    return u2f(u);
+}
